19ed7d44577fa7cc5fe7f3484c93bd15e606f863
salomonalb <95834451+salomonalb@users.noreply.github.com>
2022-11-23 13:31:48 -0400

    navigation functionality working on mobile version

--- src/ts/navigation.ts
+++ src/ts/navigation.ts
@@ -1,10 +1,9 @@
+//Images
 import openIcon from '../images/icon-hamburger.svg';
 import closeIcon from '../images/icon-close-menu.svg';
 
-export default function navigation() {
-
 //Elements
-const modalBackground = document.querySelector('#modal-background') as HTMLSpanElement;
+const navBackground = document.querySelector('#nav-background') as HTMLSpanElement;
 
 const navMenu = document.querySelector('#nav-menu') as HTMLMenuElement;
 
@@ -12,24 +11,36 @@ const navButton  = document.querySelector('#nav-button') as HTMLButtonElement;
 
 const navButtonImg = document.querySelector('#nav-button-img') as HTMLImageElement;
 
-//Images
+//Functions
+function openMenu() {
 
+    navButton.ariaExpanded = "true";
+    navButtonImg.src = closeIcon;
+    navBackground.style.display = "block";
+    navMenu.style.display = "flex";
+}
+
+function closeMenu() {
+    
+    navButton.ariaExpanded = "false";
+    navButtonImg.src = openIcon
+    navBackground.style.display = "none";
+    navMenu.style.display = "none";
+}
 
 //Events
 navButton.addEventListener('click', () => {
-    
-    if (navButton.ariaExpanded = "false") {
-        
-        modalBackground.style.display = "block";
-        navMenu.style.display = "flex";
-        navButtonImg.src = closeIcon;
-        navButton.ariaExpanded = "true";
-    } else {
-        modalBackground.style.display = "none";
-        navMenu.style.display = "none";
-        navButtonImg.src = openIcon;
-        navButton.ariaExpanded = "false";
+    if (navButton.ariaExpanded === "false") {
+
+        openMenu()
+
+    } else if (navButton.ariaExpanded === "true") {
+
+        closeMenu()
+
     }
-    
 })
-}
+
+navBackground.addEventListener('click', () => {
+    closeMenu()
+})
